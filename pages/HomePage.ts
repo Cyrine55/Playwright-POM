096b180d237e3a1756d8866eabc9f9a8c021869d
yefcyrine <yefrni.syrine@gmail.com>
2025-07-16 13:59:30 +0100

    add keyword driven approch for click,fill,wait functionnality on Base class

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,26 +1,27 @@
 import { Locator, Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
 
 
-export class HomePage{
-private page :Page;
+export class HomePage extends BasePage{
+
 
 constructor(page:Page){
-    this.page=page;
+    super(page);
 }
 
-private newCarMenu=()=>this.page.locator('//div[normalize-space()="NEW CARS"]');
-private findCarMenu=()=>this.page.locator('//div[contains(text(),"Find New Cars")]');
+//private newCarMenu=()=>this.page.locator('//div[normalize-space()="NEW CARS"]');
+//private findCarMenu=()=>this.page.locator('//div[contains(text(),"Find New Cars")]');
 
 async navigationHome(){
-    await this.page.goto('/');
+    await this.navigateTo('/');
 }
 
 async findCar(){
-    await this.newCarMenu().waitFor({ state: 'visible' });
-    await this.newCarMenu().hover();
-    await this.findCarMenu().waitFor({ state: 'visible' });
-    await this.findCarMenu().click();
-    await this.page.waitForTimeout(2000);
+    //await this.newCarMenu().waitFor({ state: 'visible' });
+    await this.hover('//div[normalize-space()="NEW CARS"]');
+   // await this.findCarMenu().waitFor({ state: 'visible' });
+    await this.click('//div[contains(text(),"Find New Cars")]');
+    await this.waitForTimeout(2000);
 
 }
 
